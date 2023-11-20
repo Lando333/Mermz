49e08519989662dbcebccf881bdfa2a2733b7c28
Lando333 <landoslair@gmail.com>
2023-11-20 13:41:13 -0500

    Updated Player Camera
    Added serializable limits,
    Added movement dampening for smoother experience

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -7,9 +7,20 @@ public class CameraMovement : MonoBehaviour
     public Transform target;
     public Vector3 offset;
 
+    [SerializeField] float leftLimit;
+    [SerializeField] float bottomLimit;
+    [SerializeField] float rightLimit;
+    [SerializeField] float topLimit;
+
+    [SerializeField] float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
 
     void Update()
     {
-        transform.position = target.position + offset;
+        Vector3 targetPosition = target.position + offset;
+        targetPosition.x = Mathf.Clamp(targetPosition.x, leftLimit, rightLimit);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, bottomLimit, topLimit);
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
