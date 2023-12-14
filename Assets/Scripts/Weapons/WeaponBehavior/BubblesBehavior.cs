2000cafcda7af3a4b919db93bcf9a09babc247e3
Lando333 <landoslair@gmail.com>
2023-12-14 14:43:32 -0500

    Added float to bubbles

--- Assets/Scripts/Weapons/WeaponBehavior/BubblesBehavior.cs
+++ Assets/Scripts/Weapons/WeaponBehavior/BubblesBehavior.cs
@@ -6,6 +6,11 @@ public class BubblesBehavior : ProjectileWeaponBehavior
 {
     List<GameObject> markedEnemies;
 
+    [SerializeField] float speedDecayRate = 0.1f; // Adjust the rate of speed decay
+    [SerializeField] float floatUpSpeed = 1.0f; // Adjust the speed at which bubbles float up after stopping
+
+    private bool hasStoppedMoving = false;
+
     protected override void Start()
     {
         base.Start();
@@ -14,7 +19,27 @@ public class BubblesBehavior : ProjectileWeaponBehavior
 
     void Update()
     {
-        transform.position += direction * currentSpeed * Time.deltaTime; //Sets the movement of the harpoon
+        if (!hasStoppedMoving)
+        {
+            // Decrement the speed over time
+            currentSpeed -= speedDecayRate * Time.deltaTime;
+
+            // Ensure the speed doesn't go below zero
+            currentSpeed = Mathf.Max(currentSpeed, 0f);
+
+            transform.position += direction * currentSpeed * Time.deltaTime; // Sets the movement of the harpoon
+
+            // Check if the bubbles have stopped moving
+            if (currentSpeed <= 0.25f)
+            {
+                hasStoppedMoving = true;
+            }
+        }
+        else
+        {
+            // Float the bubbles up after they stop moving
+            transform.position += Vector3.up * floatUpSpeed * Time.deltaTime;
+        }
     }
 
     protected override void OnTriggerEnter2D(Collider2D col)
@@ -24,7 +49,7 @@ public class BubblesBehavior : ProjectileWeaponBehavior
             EnemyStats enemy = col.GetComponent<EnemyStats>();
             enemy.TakeDamage(GetCurrentDamage());
 
-            markedEnemies.Add(col.gameObject); // Marks enemy so they don't take damage from same instance of ink
+            markedEnemies.Add(col.gameObject); // Marks enemy so they don't take damage from same instance of bubbles
         }
         else if (col.CompareTag("Prop"))
         {
