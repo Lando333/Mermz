c05934d06e39ac18b952f79d295c16fbc0d54d20
Lando333 <landoslair@gmail.com>
2023-12-23 13:16:58 -0500

    Fixed bonus gem

--- Assets/Scripts/MenuGem.cs
+++ Assets/Scripts/MenuGem.cs
@@ -8,12 +8,16 @@ public class MenuGem : MonoBehaviour
     Color blue = new(0.2039216f, 0.6156863f, 1f, 0.3254902f);
     Color purple = new(0.8f, 0.2039216f, 255, 0.3254902f);
     public int counter;
-    private bool bonusAvailable = true;
+
+    private string bonus = "Bonus";
+    private int bonusAvailable;
 
     private void Awake()
     {
         sprite = GetComponent<SpriteRenderer>();
         sprite.color = blue;
+
+        bonusAvailable = PlayerPrefs.GetInt(bonus);
     }
 
     private void OnMouseDown()
@@ -23,16 +27,21 @@ public class MenuGem : MonoBehaviour
         if (sprite.color == blue) sprite.color = purple;
         else if (sprite.color == purple) sprite.color = blue;
 
-        if (counter > 10 && bonusAvailable)
+        if (counter > 1000 && bonusAvailable == 0)
         {
-            // something cool happens
-
             CurrencyManager cm;
             cm = FindObjectOfType<CurrencyManager>();
             cm.AddCurrency(1000);
 
-            // Set in PlayerPrefs
-            bonusAvailable = false;
+            SetBonusUnavailable();
         }
+    }
+
+    public void SetBonusUnavailable()
+    {
+        PlayerPrefs.SetInt(bonus, 1);
+        PlayerPrefs.Save();
+
+        bonusAvailable = PlayerPrefs.GetInt(bonus);
     }
 }
