412cffbc020fd8304b38d70b714a1d63f47d0b3c
Lando333 <landoslair@gmail.com>
2023-12-30 20:37:33 -0500

    Added character silhouettes

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,37 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] private GameObject mermaidBox;
-    [SerializeField] private GameObject diverBox;
-    [SerializeField] private GameObject octopusManBox;
+    [Header("Setup")]
+    [SerializeField] private GameObject selectorBox;
+    [SerializeField] private TextMeshProUGUI flavorText;
+
+    [Header("Characters")]
+    [SerializeField] private GameObject diverSilhouette;
+    [SerializeField] private TextMeshProUGUI diverName;
+    [SerializeField] private GameObject octopusManSilhouette;
+    [SerializeField] private TextMeshProUGUI octopusManName;
+
+    bool diverUnlocked;
+    bool octoManUnlocked;
+
+    private void Start()
+    {
+        LoadPlayableCharacters();
+        LoadFlavorText();
+    }
 
     private void Update()
     {
         if (CharacterSelector.instance.characterData.name == "Mermaid Character")
         {
-            mermaidBox.SetActive(true);
-            diverBox.SetActive(false);
-            octopusManBox.SetActive(false);
+            selectorBox.transform.position = new Vector3(440f, 427f, selectorBox.transform.position.z);
         }
         else if (CharacterSelector.instance.characterData.name == "Diver Character")
         {
-            mermaidBox.SetActive(false);
-            diverBox.SetActive(true);
-            octopusManBox.SetActive(false);
+            selectorBox.transform.position = new Vector3(640f, 427f, selectorBox.transform.position.z);
         }
         else if (CharacterSelector.instance.characterData.name == "Octopus Man Character")
         {
-            mermaidBox.SetActive(false);
-            diverBox.SetActive(false);
-            octopusManBox.SetActive(true);
+            selectorBox.transform.position = new Vector3(840f, 427f, selectorBox.transform.position.z);
         }
     }
 
     public void SelectCharacter(CharacterScriptableObject character)
     {
         CharacterSelector.instance.characterData = character;
+        LoadFlavorText();
+    }
+
+    public void LoadPlayableCharacters() 
+    {
+        if (diverUnlocked)
+        {
+            diverSilhouette.SetActive(false);
+            diverName.text = "Dan";
+        }
+        else
+        {
+            diverSilhouette.SetActive(true);
+            diverName.text = "???";
+        }
+
+        if (octoManUnlocked)
+        {
+            octopusManSilhouette.SetActive(false);
+            octopusManName.text = "Octopus Man";
+        }
+        else
+        {
+            octopusManSilhouette.SetActive(true);
+            octopusManName.text = "???";
+        }
+    }
+
+    public void LoadFlavorText()
+    {
+        flavorText.text = CharacterSelector.instance.characterData.CharacterFlavorText;
     }
 }
